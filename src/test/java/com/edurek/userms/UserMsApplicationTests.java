aca4e13f2cb0012f031aa075dcfb6691eb885595
Ramakrishna K <ramakrishna.karneedi@gmail.com>
2018-04-11 06:50:16 +0800

    test

--- src/test/java/com/edurek/userms/UserMsApplicationTests.java
+++ src/test/java/com/edurek/userms/UserMsApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//This is to test the userms app 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserMsApplicationTests {
